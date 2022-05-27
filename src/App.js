f358139f9da54a8a283c8686c4bbe3c4dd3bde95
Armen <armen.aslikyan@redkite.io>
2022-05-27 15:11:46 +0400

    test

--- src/App.js
+++ src/App.js
@@ -1,42 +1,34 @@
-import { useLayoutEffect } from 'react';
+import * as React from "react";
 import './App.css';
+import { useAddToHomescreenPrompt } from "./useAddToHomescreenPrompt";
 
 
 function App() {
-  useLayoutEffect(() => {
-    let deferredPrompt;
-const addBtn = document.querySelector('.add-button');
-// addBtn.style.display = 'none';
 
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent Chrome 67 and earlier from automatically showing the prompt
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI to notify the user they can add to home screen
-  addBtn.style.display = 'block';
+  const [prompt, promptToInstall] = useAddToHomescreenPrompt();
+  const [isVisible, setVisibleState] = React.useState(false);
 
-  addBtn.addEventListener('click', () => {
-    // hide our user interface that shows our A2HS button
-    // addBtn.style.display = 'none';
-    // Show the prompt
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choiceResult) => {
-      if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
+  const hide = () => setVisibleState(false);
+
+  React.useEffect(
+    () => {
+      if (prompt) {
+        setVisibleState(true);
       }
-      deferredPrompt = null;
-    });
-  });
-});
-  }, [])
+    },
+    [prompt]
+  );
+
+  if (!isVisible) {
+    return <div />;
+  }
+
   return (
-    <div className="App">
-1
-      <button className="add-button">Add to home screen</button>
+    <div onClick={hide}>
+      2
+      <button onClick={hide}>Close</button>
+      Hello! Wanna add to homescreen?
+      <button onClick={promptToInstall}>Add to homescreen</button>
     </div>
   );
 }
