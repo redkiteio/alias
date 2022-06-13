540b7418d67b5fde16bcb8609f3f3646c5c86974
gora <gora@redkite.io>
2022-06-13 11:27:18 +0400

    game integration

---
+++ src/alias/words.js
@@ -0,0 +1,32 @@
+const words = [
+    {
+        phrase: "A bird in the hand is worth two in the bush",
+        description: "What you have is worth more than what you might have later",
+    },
+    {
+        phrase: "A blessing in disguise",
+        description: "a good thing that seemed bad at first",
+    },
+    {
+        phrase: "A dime a dozen",
+        description: "Something common",
+    },
+    {
+        phrase: "Beat around the bush",
+        description: "Avoid saying what you mean, usually because it is uncomfortable",
+    },
+    {
+        phrase: "Better late than never",
+        description: "Better to arrive late than not to come at all",
+    },
+    {
+        phrase: "Bite the bullet",
+        description: "To get something over with because it is inevitable",
+    },
+    {
+        phrase: "Break a leg",
+        description: "Good luck",
+    },
+];
+
+export { words };
